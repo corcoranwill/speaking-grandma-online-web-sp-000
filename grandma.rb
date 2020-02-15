f9304f7e4cb55fec974a12e331e3f8ffb3f56d13
Will Corcoran <corcoranwill@gmail.com>
2020-02-15 02:02:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,15 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
+
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    if phrase == "I LOVE YOU GRANDMA!"
+      puts "I LOVE YOU TOO PUMPKIN!"
+    else
+      puts "NO, NOT SINCE 1938!"
+    end
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
