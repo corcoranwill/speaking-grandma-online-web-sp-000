b695a4ba28a75a9c397e18dbc73e4fbb63c8c5d0
Will Corcoran <corcoranwill@gmail.com>
2020-02-15 02:03:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
-# Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else
-    puts "HUH?! SPEAK UP, SONNY!"
-  end
-end
+# # Write a speak_to_grandma method.
+# def speak_to_grandma(phrase)
+#   if phrase == phrase.upcase
+#     puts "NO, NOT SINCE 1938!"
+#   elsif phrase == "I LOVE YOU GRANDMA!"
+#     puts "I LOVE YOU TOO PUMPKIN!"
+#   else
+#     puts "HUH?! SPEAK UP, SONNY!"
+#   end
+# end
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
