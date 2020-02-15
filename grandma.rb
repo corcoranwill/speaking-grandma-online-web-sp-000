b40a00b246eb7909147023e8e15e72bad84457de
Will Corcoran <corcoranwill@gmail.com>
2020-02-15 01:48:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,16 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase!
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end 
+end 
+
+
+
+
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
